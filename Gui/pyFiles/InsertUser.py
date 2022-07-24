366fc9d585d2c09f0e6502a84d9142223280804b
compguy123 <77632039+compguy123@users.noreply.github.com>
2022-07-23 20:24:36 -0500

    new fixes 1

--- Gui/pyFiles/InsertUser.py
+++ Gui/pyFiles/InsertUser.py
@@ -1,14 +1,8 @@
-from kivy.app import App
-from kivy.lang.builder import Builder
-from kivy.uix.screenmanager import Screen, ScreenManager
 from kivy.properties import ObjectProperty
+from datetime import datetime
+from kivy.uix.screenmanager import Screen
 from kivy.uix.popup import Popup
 from kivy.uix.label import Label
-from datetime import datetime
-
-
-class Homepage(Screen):
-    pass
 
 
 class InsertUser(Screen):
@@ -21,7 +15,6 @@ class InsertUser(Screen):
         if self.userName.text != "" and self.validateDate():
 
             self.reset()
-            windowManager.current = "ShowUser"
         else:
             invalidUser()
 
@@ -32,8 +25,9 @@ class InsertUser(Screen):
         self.dob.text = ""
 
     def homepage(self):
-        # return to homepage
-        windowManager.current = "Homepage"
+        pass
+
+    # return to homepage
 
     # check date
     def validateDate(self):
@@ -44,38 +38,9 @@ class InsertUser(Screen):
             return False
 
 
-class ShowUser(Screen):
-    pass
-
-
-class WindowManager(ScreenManager):
-    pass
-
-
-# load kv file
-kv = Builder.load_file("my.kv")
-
-# screen manager
-windowManager = WindowManager()
-
-# change screens in code
-screens = {Homepage(name="Homepage"), InsertUser(name="InsertUser"), ShowUser(name="ShowUser")}
-for screen in screens:
-    windowManager.add_widget(screen)
-
-
 # create pipup
 def invalidUser():
     pop = Popup(title='Error',
                 content=Label(text='Invalid name or date.'),
                 size_hint=(.4, .4))
     pop.open()
-
-
-class MyApp(App):
-    def build(self):
-        return windowManager
-
-
-if __name__ == "__main__":
-    MyApp().run()
---
+++ Gui/pyFiles/Main.py
@@ -0,0 +1,16 @@
+from kivy.lang import Builder
+
+from Gui.pyFiles.WindowManager import WindowManager
+from kivy.app import App
+
+
+class MyApp(App):
+    def build(self):
+        windowManager = WindowManager()
+        # change screens in code
+        Builder.load_file("../kvFiles/main.kv")
+        windowManager.initalize()
+        return windowManager
+
+if __name__ == "__main__":
+    MyApp().run()
